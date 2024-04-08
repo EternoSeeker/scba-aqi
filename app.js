e2b7fbf815620a2f8ca45aa1551c326e8807c65b
Shrihari Magar <shriharimagar1@gmail.com>
2024-04-09 00:22:12 +0530

    feat: lat long from api

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
+
+var city = 'london'
+var country = 'India'
+
 document.addEventListener('DOMContentLoaded', function() {
+    $.ajax({
+        method: 'GET',
+        url: "https://api.api-ninjas.com/v1/geocoding?city=" + city + "&country=" + country,
+        headers: { 'X-Api-Key': 'y3qqgPjhSxvsv8WE6x7A3A==XqFvISvIxEARkHIX'},
+        contentType: 'application/json',
+        success: function(result) {
+            console.log(result);
+        },
+        error: function ajaxError(jqXHR) {
+            console.error('Error: ', jqXHR.responseText);
+        }
+    });
+    
     var map = new google.maps.Map(document.getElementById('map'), {
         center: new google.maps.LatLng(19.082502, 72.7163719),
         mapTypeId: google.maps.MapTypeId.ROADMAP,
